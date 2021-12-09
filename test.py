88dc502f9f0371f1301c613f4eb310bd43412bb6
Saisree-123 <saisree.gr@gmail.com>
2021-12-08 16:55:26 -0800

    please work

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
-from unit_test.test_basicmembersip import TestBasicMembersip
 from unit_test.test_freemembership import TestFreeMembersip
+from unit_test.test_basicmembersip import TestBasicMembersip
 from unit_test.test_premiummembership import TestPremiumMembersip
 from unit_test.test_user_bankinfo import test_user_bank_info
 from unit_test.test_user_personal_info import test_user_personal_info
@@ -13,7 +13,7 @@ from unit_test.test_user_service import test_user_service
 def my_suite():
     suite = unittest.TestSuite()
     result = unittest.TestResult()
-    tests=[TestBasicMembersip,TestFreeMembersip,TestPremiumMembersip,test_user_bank_info,test_user_personal_info,test_user_service]
+    tests=[TestFreeMembersip,TestBasicMembersip,TestPremiumMembersip,test_user_bank_info,test_user_personal_info,test_user_service]
     for test in tests:
         suite.addTest(unittest.makeSuite(test))    
     runner = unittest.TextTestRunner()
