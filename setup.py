86134f10e87c006cf4e9671712ff0725a1c25392
TingwenH <tingwenh@student.ubc.ca>
2021-12-11 14:26:31 -0800

    Update Setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='budgetplanner',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['test*']),
     license='MIT',
     description='When piggy bank becomes your financial assisstant',
 	url='https://github.com/Saisree-123/DATA533-LAB4',
-    author='Saisree GR',
-    author_email='saisree.gr@gmail.com'
+    author='Saisree GR, Tingwen Hua',
+    author_email='saisree.gr@gmail.com, tingwenhua@gmail.com'
 )
