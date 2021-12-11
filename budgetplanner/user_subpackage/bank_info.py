c9be68e6fba160f1a2ff1482b0eee1769f4a425e
TingwenH <tingwenh@student.ubc.ca>
2021-12-11 13:09:21 -0800

    Try-Exception

--- budgetplanner/user_subpackage/bank_info.py
+++ budgetplanner/user_subpackage/bank_info.py
@@ -1,8 +1,23 @@
 class password:
-
     def __init__(self,userid,password):
         self.userid = userid
         self.password = password
+        class Error(Exception):
+            pass
+        class PasswordTooSmallError(Error):
+            pass
+        class PasswordTooLargeError(Error):
+            pass
+        try:
+            if len(self.password) < 7:
+                raise PasswordTooSmallError
+            elif len(self.password) > 15:
+                raise PasswordTooLargeError
+        except PasswordTooSmallError:
+            print("Password lenth is too small.")
+        except PasswordTooLargeError:
+            print("Password lenth is too large.")
+        
     def update(self,newuserid, newpassword):
         self.userid = newuserid
         self.password = newpassword
--- budgetplanner/user_subpackage/service.py
+++ budgetplanner/user_subpackage/service.py
@@ -20,32 +20,48 @@ class Service:
         print('1.Bank information storage 2. Simple savings tracker 3.Basic budget Planning 4.Detailed analysis and customized suggestion')
         
     def show(self,service_number):
-        if service_number == 1:
-            print('This service can store your bank informations, such as account/online banking id and password.')
-        elif service_number == 2:
-            print('This service can track your monthly expenditures and help you manage your money and making a monthly budget plan.')
-        elif service_number == 3:
-            print('This service can help you create a budget planning based on your income and daily usage.')
-        elif service_number == 4:
-            print('This service can give you some detailed analysis and suggestions on how to save your money.')
-        else:
-            print('The service you chose is not available.Please provide a vaild service number.')
-
+        class Error(Exception):
+            pass
+        class NoserviceError(Error):
+            print('Please provide a vaild service number.')
+        try:
+            if service_number == 1:
+                 print('This service can store your bank informations, such as account/online banking id and password.')
+            elif service_number == 2:
+                 print('This service can track your monthly expenditures and help you manage your money and making a monthly budget plan.')
+            elif service_number == 3:
+                 print('This service can help you create a budget planning based on your income and daily usage.')
+            elif service_number == 4:
+                 print('This service can give you some detailed analysis and suggestions on how to save your money.')
+            else:
+                raise NoserviceError
+        except NoserviceError as e:
+                 print(e)  
+        
+            
     def choice(self,c_num):
-        if c_num == 1:
-            print('This service is included in free membership.')
-            self.__choice=c_num
-        elif c_num == 2:
-            print('This service is include in free membership.')
-            self.__choice=c_num
-        elif c_num == 3:
-            print('This service is included in basic membership. The monthly sibscription fee is $15.')
-            self.__choice=c_num
-        elif c_num == 4:
-            print('This service is included in premium membership. The monthly sibscription fee is $25.')
-            self.__choice=c_num
-        else:
-            print('The service you chose is not available.Please provide a vaild service number.')
+        class Error(Exception):
+            pass
+        class NoserviceError(Error):
+            print('Please provide a vaild service number.')
+        try:
+            if c_num == 1:
+                print('This service is included in free membership.')
+                self.__choice=c_num
+            elif c_num == 2:
+                print('This service is include in free membership.')
+                self.__choice=c_num
+            elif c_num == 3:
+                print('This service is included in basic membership. The monthly sibscription fee is $15.')
+                self.__choice=c_num
+            elif c_num == 4:
+                print('This service is included in premium membership. The monthly sibscription fee is $25.')
+                self.__choice=c_num
+            else:
+                raise NoserviceError
+        except NoserviceError as e:
+            print(e)
+            
 
     def getChoice(self):
         return self.__choice
